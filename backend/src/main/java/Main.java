e4dc715469e8de254da46ff562fbbbbf38157c52
zohar <kozohar@gmail.com>
2017-01-07 19:54:18 +0200

    added backend as maven project

---
+++ backend/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.apache.log4j.Logger;
+import spark.Spark;
+
+public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
